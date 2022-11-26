0b02f89b6777f194f6e6c7baf56af61b816e7feb
PedroMakengo <pedromakengomiguel@gmail.com>
2022-11-26 08:49:13 +0100

    Trabalhando

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Url da api: https://sujeitoprogramador.com/rn-api/?api=posts
-const api = axios.create({
-  baseURL: "https://sujeitoprogramador.com/rn-api/",
-});
-
-export default api;
