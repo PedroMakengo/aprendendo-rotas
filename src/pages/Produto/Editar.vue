1fd2a2a4b627f392a727b96c0edac57246301754
PedroMakengo <pedromakengomiguel@gmail.com>
2022-11-26 11:52:00 +0100

    Trabalhando

--- src/pages/Produto/Editar.vue
+++ src/pages/Produto/Editar.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div id="detalhe-produto">
-    <h2>Detalhe do produto</h2>
+  <div id="editar-produto">
+    <h2>Editar do produto</h2>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import Produto from "../pages/Produto/index.vue";
 import MeusProdutos from "../pages/Produto/MeusProdutos.vue";
+import Editar from "../pages/Produto/Editar.vue";
+import Detalhe from "../pages/Produto/Detalhe.vue";
 
 const routes = [
   {
@@ -13,11 +15,15 @@ const routes = [
     path: "/produto",
     name: "Produto",
     component: Produto,
-  },
-  {
-    path: "/meus-produtos",
-    name: "MeusProdutos",
-    component: MeusProdutos,
+    children: [
+      { path: "/", component: MeusProdutos },
+      { path: "/:id", component: Detalhe, props: true },
+      {
+        path: "/:id/editar",
+        component: Editar,
+        props: true,
+      },
+    ],
   },
 ];
 
